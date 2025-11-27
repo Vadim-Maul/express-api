8d819f793301f3a0139e81d33aa3f30d2f37b73c
Vadim <vadim.maul@cutvert.de>
2025-11-27 15:12:06 +0100

    feat: implement App class with server initialization and create main entry point

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,18 @@
+import express, { Express } from "express";
+import { Server } from "http";
+
 export class App {
+  app: Express;
+  server!: Server;
+  port: number;
+
   constructor() {
-    console.log("App initialized");
+    this.app = express();
+    this.port = 3000;
+  }
+
+  public async init() {
+    this.server = this.app.listen(this.port);
+    console.log(`Server is running on http://localhost:${this.port}`);
   }
 }
