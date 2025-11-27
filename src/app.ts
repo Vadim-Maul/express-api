e7c6380901701ddd82a85917166e21b5ba357c07
Vadim <vadim.maul@cutvert.de>
2025-11-27 15:32:37 +0100

    feat: add LoggerService for improved logging and integrate it into App class

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import express, { Express } from "express";
 import { Server } from "http";
+import { LoggerService } from "./logger/logger.service";
 
 export class App {
   app: Express;
   server!: Server;
   port: number;
+  logger: LoggerService;
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 3000;
+    this.logger = logger;
   }
 
   public async init() {
     this.server = this.app.listen(this.port);
-    console.log(`Server is running on http://localhost:${this.port}`);
+    this.logger.log(`Server is running on http://localhost:${this.port}`);
   }
 }
