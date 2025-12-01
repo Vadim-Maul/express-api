558eaaff15b21398b2e024d3787deec0c71f1253
Vadim <vadim.maul@cutvert.de>
2025-12-01 17:37:06 +0100

    fix: format code in RolesGuard and IProductService interface for consistency

--- src/products/service/product.service.interface.ts
+++ src/products/service/product.service.interface.ts
@@ -3,9 +3,9 @@ import { CreateProductDto } from '../dto/create-product.dto';
 import { UpdateProductDto } from '../dto/update-product.dto';
 
 export interface IProductService {
-  create: (dto: CreateProductDto) => Promise<ProductModel>;
-  findById: (id: number) => Promise<ProductModel | null>;
-  findAll: () => Promise<ProductModel[]>;
-  update: (id: number, dto: UpdateProductDto) => Promise<ProductModel | null>;
-  delete: (id: number) => Promise<boolean>;
+	create: (dto: CreateProductDto) => Promise<ProductModel>;
+	findById: (id: number) => Promise<ProductModel | null>;
+	findAll: () => Promise<ProductModel[]>;
+	update: (id: number, dto: UpdateProductDto) => Promise<ProductModel | null>;
+	delete: (id: number) => Promise<boolean>;
 }
